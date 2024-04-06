c480b65bebf24673c5c5616de4c38224c6f70a63
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-04-06 17:21:50 -0300

    initial files

---
+++ src/main/java/br/com/hrmaster/HrMasterApplication.java
@@ -0,0 +1,13 @@
+package br.com.hrmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hrmaster/HrMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hrmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
